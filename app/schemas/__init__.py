9ff51fbae44dc16a68ef8fe36ec3c669f9da140b
kao264918 <kao264918@gmail.com>
2026-01-13 14:48:39 +0800

    fix: export missing CatalogItemListResponse schema

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,6 @@
 from .customer import Customer, CustomerCreate, CustomerUpdate, CustomerListResponse
 from .vendor import Vendor, VendorCreate, VendorUpdate, VendorContactBase
-from .catalog import CatalogItem, CatalogItemCreate, CatalogItemUpdate
+from .catalog import CatalogItem, CatalogItemCreate, CatalogItemUpdate, CatalogItemListResponse, CatalogItemMeta, CatalogItemInternal
 from .tax_category import TaxCategory, TaxCategoryCreate, TaxCategoryUpdate
 from .unit import Unit, UnitCreate, UnitUpdate
 from .template import QuoteTemplate, InvoiceTemplate
